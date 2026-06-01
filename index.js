5d962f057febca721c0b3686796ef6389424b9bb
Divyansh <rajoriadivyansh2004@gmail.com>
2026-06-01 15:14:16 +0530

    commit initial

---
+++ index.js
@@ -0,0 +1,14 @@
+let saveel = document.getElementById("save-el")
+let countel = document.getElementById("counter")
+let count = 0
+
+function increment() {
+    count += 1
+    countel.textContent = count
+}
+function save(){
+    let countstr = count + " - "
+    saveel.textContent += countstr
+    countel.textContent = 0
+    count = 0
+}
